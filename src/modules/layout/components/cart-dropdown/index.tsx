a9d12639eed6a54383a4c83f8d9d6d180e2ea521
kracas <karolis@lobot.co>
2023-04-12 14:49:44 +0000

    top menu

--- src/modules/layout/components/cart-dropdown/index.tsx
+++ src/modules/layout/components/cart-dropdown/index.tsx
@@ -21,7 +21,18 @@ const CartDropdown = () => {
     <div className="h-full z-50" onMouseEnter={open} onMouseLeave={close}>
       <Popover className="relative h-full">
         <Link href="/cart" passHref>
-          <Popover.Button className="h-full">{`My Bag (${totalItems})`}</Popover.Button>
+          <Popover.Button className="h-full">
+            <div className="mr-2">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+              </svg>
+              <span className="absolute inset-0 -mr-2 mt-8">
+                <div className="inline-flex items-center px-1.5 py-0.5 border-1 border-white rounded-full text-xs font-semibold leading-4 bg-black text-white">
+                  {totalItems}
+                </div>
+              </span>
+            </div>
+          </Popover.Button>
         </Link>
         <Transition
           show={state}
--- src/modules/layout/templates/nav/index.tsx
+++ src/modules/layout/templates/nav/index.tsx
@@ -8,58 +8,33 @@ import clsx from "clsx"
 import Link from "next/link"
 import { useRouter } from "next/router"
 import { useEffect, useState } from "react"
+import useEmblaCarousel from "embla-carousel-react"
+import Autoplay from "embla-carousel-autoplay"
+import Image from "next/image"
+import logo from "@assets/logo.png"
 
 const Nav = () => {
-  const { pathname } = useRouter()
-  const [isHome, setIsHome] = useState(false)
-  const [isScrolled, setIsScrolled] = useState(false)
 
-  //useEffect that detects if window is scrolled > 5px on the Y axis
-  useEffect(() => {
-    if (isHome) {
-      const detectScrollY = () => {
-        if (window.scrollY > 5) {
-          setIsScrolled(true)
-        } else {
-          setIsScrolled(false)
-        }
-      }
-
-      window.addEventListener("scroll", detectScrollY)
-
-      return () => {
-        window.removeEventListener("scroll", detectScrollY)
-      }
-    }
-  }, [isHome])
-
-  useEffect(() => {
-    pathname === "/" ? setIsHome(true) : setIsHome(false)
-  }, [pathname])
+  const [emblaRef] = useEmblaCarousel({ loop: true }, [Autoplay({ delay: 3000, stopOnInteraction: false })]);
 
   const { toggle } = useMobileMenu()
 
   return (
     <div
-      className={clsx("sticky top-0 inset-x-0 z-50 group", {
-        "!fixed": isHome,
-      })}
+      className="sticky top-0 inset-x-0 z-50 group"
     >
+      <div className="overflow-hidden text-center text-sm py-1 bg-black text-white" ref={emblaRef}>
+        <div className="flex">
+          <div className="relative flex-[0_0_100%]"><Link href='/'><a>Text</a></Link></div>
+          <div className="relative flex-[0_0_100%]"><Link href='/'><a>Text 2</a></Link></div>
+
+        </div>
+      </div>
       <header
-        className={clsx(
-          "relative h-16 px-8 mx-auto transition-colors bg-transparent border-b border-transparent duration-200 group-hover:bg-white group-hover:border-gray-200",
-          {
-            "!bg-white !border-gray-200": !isHome || isScrolled,
-          }
-        )}
+        className="relative h-16 px-4 mx-auto transition-colors bg-transparent border-b border-transparent duration-200 group-hover:bg-white group-hover:border-gray-200 !bg-white !border-gray-200"
       >
         <nav
-          className={clsx(
-            "text-gray-900 flex items-center justify-between w-full h-full text-small-regular transition-colors duration-200",
-            {
-              "text-white group-hover:text-gray-900": isHome && !isScrolled,
-            }
-          )}
+          className="text-gray-900 flex items-center justify-between w-full h-full text-small-regular transition-colors duration-200"
         >
           <div className="flex-1 basis-0 h-full flex items-center">
             <div className="block small:hidden">
@@ -70,9 +45,11 @@ const Nav = () => {
             </div>
           </div>
 
-          <div className="flex items-center h-full">
+          <div className="flex items-center h-full w-full relative">
             <Link href="/">
-              <a className="text-xl-semi uppercase">Acme</a>
+              <a>
+                <Image src={logo} objectFit="contain" alt="" layout="fill" />
+              </a>
             </Link>
           </div>
 
