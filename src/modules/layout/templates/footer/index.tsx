2d4481c62a35141d05865efc1b85f19f1a696905
kracas <karolis@lobot.co>
2023-04-18 09:47:59 +0000

    remove unused import from footer

--- src/modules/layout/templates/footer/index.tsx
+++ src/modules/layout/templates/footer/index.tsx
@@ -1,6 +1,5 @@
 import FooterCTA from "@modules/layout/components/footer-cta"
 import FooterNav from "@modules/layout/components/footer-nav"
-import MedusaCTA from "@modules/layout/components/medusa-cta"
 
 const Footer = () => {
   return (
