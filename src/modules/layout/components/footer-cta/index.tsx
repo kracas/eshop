b3add3c1660bbdb70e7713c0493ad1f2b2a40d7a
kracas <karolis@lobot.co>
2023-05-01 16:46:51 +0000

    fix instafeed

--- src/modules/layout/components/footer-cta/index.tsx
+++ src/modules/layout/components/footer-cta/index.tsx
@@ -7,21 +7,23 @@ const FooterCTA = () => {
   useEffect(() => {
     const feed = new Instafeed({
       accessToken: 'IGQVJYMlRmaS1vMTVrOE5heDBrbHdBSmJlNmFleExGWGl0SE9qZA3pBUmpTQ3pUendtQWRBOEpOTXJFd01QZAHJLTGROdkVIcmZAJSU1iZAFhpY3d3SXJ2QmR6UnJJNmpZAODBtV0VWRFdWWFZAJb2s5dU5NMQZDZD',
-      limit: 6
+      limit: 6,
+      template: '<a href="{{link}}" target="_blank"><img class="w-24 h-24 md:w-60 md:h-60 object-cover" title="{{caption}}" src="{{image}}" /></a>',
     });
     feed.run();
   }, [])
 
   return (
     <div className="bg-red-50 w-full">
-      <div className="content-container flex flex-col-reverse gap-y-8 small:flex-row small:items-center justify-between py-8 relative">
-        <div>
-          <h3 className="text-2xl-semi">Shop the latest styles</h3>
-          <div className="mt-6">
-            <UnderlineLink href="/store">Explore products</UnderlineLink>
-          </div>
+      <h3 className="text-2xl text-center pt-8"><a href="https://www.instagram.com/foreverseptember.lt/" target="_blank">Insta Stories</a></h3>
+      <div className="flex justify-center py-4">
+        <div id="instafeed" className="flex flex-wrap gap-1 md:gap-3 flex-row justify-center justify-items-center max-w-xs md:max-w-4xl" />
+      </div>
+      <div>
+        <h3 className="text-2xl-semi text-center">Shop the latest styles</h3>
+        <div className="pb-6">
+          <UnderlineLink href="/store">Explore products</UnderlineLink>
         </div>
-        <div id="instafeed" className="flex flex-wrap gap-3"/>
       </div>
     </div>
   )
