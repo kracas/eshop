1eae19a0b0ce7127b4a646dc9684c7e51d088ea4
kracas <karolis@lobot.co>
2023-05-01 17:07:50 +0000

    add dynamic link to hero slide

--- src/modules/home/components/hero/slide.tsx
+++ src/modules/home/components/hero/slide.tsx
@@ -2,16 +2,16 @@ import UnderlineLink from "@modules/common/components/underline-link"
 import Image from "next/legacy/image"
 import { StaticImageData } from "next/legacy/image"
 
-const Slide: React.FC<{ image: StaticImageData, text: string, linkText: string }> = ({ image, text, linkText }) => {
+const Slide: React.FC<{ image: StaticImageData, text: string, linkText: string, linkUrl: string, }> = ({ image, text, linkText, linkUrl }) => {
 
   return (
     <div className="relative flex-[0_0_100%]">
       <div className="h-[90vh] w-full relative">
-        <div className="text-white absolute inset-0 z-10 mb-14 flex flex-col justify-end items-center text-center small:text-left small:justify-end small:items-start small:p-32">
+        <div className="text-white absolute inset-0 z-10 mb-14 flex flex-col justify-end items-center text-center">
           <h1 className="text-xl-semi mb-4 drop-shadow-md shadow-black uppercase px-10">
             {text}
           </h1>
-          <UnderlineLink href="/store">{linkText}</UnderlineLink>
+          <UnderlineLink href={linkUrl}>{linkText}</UnderlineLink>
         </div>
         <Image
           src={image}
