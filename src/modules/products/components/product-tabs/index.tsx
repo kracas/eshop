f5e89bf92cb60f5b4d38b0eab615dd245220397e
kracas <karolis@lobot.co>
2023-05-05 07:50:18 +0000

    remove product info in product page

--- src/modules/products/components/product-tabs/index.tsx
+++ src/modules/products/components/product-tabs/index.tsx
@@ -13,16 +13,16 @@ type ProductTabsProps = {
 const ProductTabs = ({ product }: ProductTabsProps) => {
   const tabs = useMemo(() => {
     return [
-      {
-        label: "Product Information",
-        component: <ProductInfoTab product={product} />,
-      },
+      // {
+      //   label: "Product Information",
+      //   component: <ProductInfoTab product={product} />,
+      // },
       {
         label: "Shipping & Returns",
         component: <ShippingInfoTab />,
       },
     ]
-  }, [product])
+  }, [])
 
   return (
     <div>
@@ -56,42 +56,42 @@ const ProductTabs = ({ product }: ProductTabsProps) => {
   )
 }
 
-const ProductInfoTab = ({ product }: ProductTabsProps) => {
-  return (
-    <Tab.Panel className="text-small-regular py-8">
-      <div className="grid grid-cols-2 gap-x-8">
-        <div className="flex flex-col gap-y-4">
-          <div>
-            <span className="font-semibold">Material</span>
-            <p>{product.material ? product.material : "-"}</p>
-          </div>
-          <div>
-            <span className="font-semibold">Country of origin</span>
-            <p>{product.origin_country ? product.origin_country : "-"}</p>
-          </div>
-          <div>
-            <span className="font-semibold">Type</span>
-            <p>{product.type ? product.type.value : "-"}</p>
-          </div>
-        </div>
-        <div className="flex flex-col gap-y-4">
-          <div>
-            <span className="font-semibold">Weight</span>
-            <p>{product.weight ? `${product.weight} g` : "-"}</p>
-          </div>
-          <div>
-            <span className="font-semibold">Dimensions</span>
-            <p>
-              {product.length && product.width && product.height
-                ? `${product.length}L x ${product.width}W x ${product.height}H`
-                : "-"}
-            </p>
-          </div>
-        </div>
-      </div>
-    </Tab.Panel>
-  )
-}
+// const ProductInfoTab = ({ product }: ProductTabsProps) => {
+//   return (
+//     <Tab.Panel className="text-small-regular py-8">
+//       <div className="grid grid-cols-2 gap-x-8">
+//         <div className="flex flex-col gap-y-4">
+//           <div>
+//             <span className="font-semibold">Material</span>
+//             <p>{product.material ? product.material : "-"}</p>
+//           </div>
+//           <div>
+//             <span className="font-semibold">Country of origin</span>
+//             <p>{product.origin_country ? product.origin_country : "-"}</p>
+//           </div>
+//           <div>
+//             <span className="font-semibold">Type</span>
+//             <p>{product.type ? product.type.value : "-"}</p>
+//           </div>
+//         </div>
+//         <div className="flex flex-col gap-y-4">
+//           <div>
+//             <span className="font-semibold">Weight</span>
+//             <p>{product.weight ? `${product.weight} g` : "-"}</p>
+//           </div>
+//           <div>
+//             <span className="font-semibold">Dimensions</span>
+//             <p>
+//               {product.length && product.width && product.height
+//                 ? `${product.length}L x ${product.width}W x ${product.height}H`
+//                 : "-"}
+//             </p>
+//           </div>
+//         </div>
+//       </div>
+//     </Tab.Panel>
+//   )
+// }
 
 const ShippingInfoTab = () => {
   return (
