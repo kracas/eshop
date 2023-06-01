a83ed69e179ca4878f989604723dd3e5247c4ba6
kracas <karolis@lobot.co>
2023-06-01 18:22:09 +0000

    change pm2 script

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,8 +2,8 @@ module.exports = {
     apps: [
       {
         name: 'storefront',
-        script: 'yarn',
-        args: 'start',
+        script: 'node_modules/next/dist/bin/next',
+        args: 'start -p 8000',
         cwd: `${process.env.PWD}/current`,
         error_file: `${process.env.PWD}/logs/app.err.log`,
         out_file: `${process.env.PWD}/logs/app.out.log`,
