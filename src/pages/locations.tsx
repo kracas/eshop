dc5058f4b2f377b8fc8f6e04c86bdf449e9c48a9
kracas <karolis@lobot.co>
2023-06-02 16:33:31 +0000

    add a tag to phone in locations

--- src/pages/locations.tsx
+++ src/pages/locations.tsx
@@ -25,7 +25,7 @@ const Locations: NextPageWithLayout = () => {
             <div className="text-xs">
               Pilies g. 16, 01123 Vilnius, Lithuania
               <br />
-              Please call before arrival: +37068763351
+              Please call before arrival: <a href="tel:+37068763351" className="underline">+37068763351</a>
             </div>
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d576.6254400966168!2d25.2888569696661!3d54.68319747358279!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46dd9416598467d1%3A0xc823690ca23686a6!2sPilies%20g.%2016%2C%2001124%20Vilnius!5e0!3m2!1sen!2slt!4v1681469909768!5m2!1sen!2slt"
