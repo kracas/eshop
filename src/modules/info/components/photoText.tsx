beeb19e1141b0af0f5add523b72cae934f00ff9d
kracas <karolis@lobot.co>
2023-06-02 16:20:20 +0000

    remove margin in photoText

--- src/modules/info/components/photoText.tsx
+++ src/modules/info/components/photoText.tsx
@@ -4,9 +4,9 @@ import { StaticImageData } from "next/legacy/image"
 const PhotoText: React.FC<{ image: StaticImageData, text: string }> = ({ image, text }) => {
 
   return (
-    <div className="relative flex-[0_0_100%] m-5">
+    <div className="relative flex-[0_0_100%]">
       <div className="h-[90vh] w-full relative">
-        <div className="text-white absolute inset-0 z-10 mb-14 flex flex-col justify-end items-center text-center small:text-left small:justify-end small:items-start small:p-32">
+        <div className="text-white absolute inset-0 z-10 mb-14 flex flex-col justify-end items-center text-center">
           <h1 className="text-xl-semi mb-4 drop-shadow-md shadow-black uppercase px-10">
             {text}
           </h1>
