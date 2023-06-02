ddb90df9d7ce485cde774957076baf2ab755b285
kracas <karolis@lobot.co>
2023-06-02 17:02:13 +0000

    center items in InfoTemplate

--- src/modules/info/templates/index.tsx
+++ src/modules/info/templates/index.tsx
@@ -10,13 +10,13 @@ const InfoTemplate: React.FC<InfoTemplateProps> = ({
 }) => {
   return (
     <div className="py-6 mt-2 mb-6 px-2">
-      <div className="content-container flex flex-col item-center text-center justify-center">
+      <div className="content-container flex flex-col items-center">
         {title &&
           <div className="text-2xl font-bold mb-10 uppercase">
             {title}
           </div>
         }
-        <div className="max-w-4xl h-full w-full flex flex-col item-center text-center text-justify gap-5 text-sm">
+        <div className="max-w-4xl h-full w-full flex flex-col gap-5 text-sm">
           {text}
         </div>
       </div>
