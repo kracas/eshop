15ddfbd5d6d3fc77dd62cc56df3bac4fce41c9e1
kracas <karolis@lobot.co>
2023-07-14 17:09:46 +0000

    track cart items add and remove

--- src/lib/context/store-context.tsx
+++ src/lib/context/store-context.tsx
@@ -1,14 +1,18 @@
 import { medusaClient } from "@lib/config"
 import { handleError } from "@lib/util/handle-error"
-import { Region } from "@medusajs/medusa"
+import { Region, LineItem } from "@medusajs/medusa"
 import {
   useCart,
   useCreateLineItem,
   useDeleteLineItem,
-  useUpdateLineItem,
+  useUpdateLineItem
 } from "medusa-react"
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState, useReducer } from "react"
 import { useCartDropdown } from "./cart-dropdown-context"
+import useEnrichedLineItems from "@lib/hooks/use-enrich-line-items"
+import { CalculatedVariant } from "types/medusa"
+import { nanoid } from 'nanoid'
+import { sendGtmEcommerceEvent } from "@lib/util/googleTagManager"
 
 interface VariantInfoProps {
   variantId: string
@@ -42,6 +46,13 @@ export const useStore = () => {
 interface StoreProps {
   children: React.ReactNode
 }
+type GtmEvent = { id: string, quantity: number, } &
+  ({ eventType: "add" | "update", prevLineItem?: undefined } |
+  { eventType: "add" | "update" | "delete", prevLineItem: Omit<LineItem, "beforeInsert"> }) &
+  (
+    { lineId: string, variantId?: undefined } |
+    { variantId: string, lineId?: undefined }
+  )
 
 const IS_SERVER = typeof window === "undefined"
 const CART_KEY = "medusa_cart_id"
@@ -50,10 +61,20 @@ const REGION_KEY = "medusa_region"
 export const StoreProvider = ({ children }: StoreProps) => {
   const { cart, setCart, createCart, updateCart } = useCart()
   const [countryCode, setCountryCode] = useState<string | undefined>(undefined)
+  const [gtmEvents, setGtmEvents] = useReducer((state: GtmEvent[], action: { action: 'add' | 'remove', event: GtmEvent }) => {
+    if (action.action === 'add') {
+      return [...state, action.event]
+    }
+    if (action.action === 'remove') {
+      return state.filter((e) => e.id !== action.event.id)
+    }
+    return state
+  }, [])
   const { timedOpen } = useCartDropdown()
   const addLineItem = useCreateLineItem(cart?.id!)
   const removeLineItem = useDeleteLineItem(cart?.id!)
   const adjustLineItem = useUpdateLineItem(cart?.id!)
+  const enrichedItems = useEnrichedLineItems()
 
   const storeRegion = (regionId: string, countryCode: string) => {
     if (!IS_SERVER) {
@@ -226,6 +247,86 @@ export const StoreProvider = ({ children }: StoreProps) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
+  useEffect(() => {
+    if (!gtmEvents.length) return
+
+    const sendGtmEvent = async (gtmEvent: GtmEvent) => {
+      const { eventType, lineId, variantId, quantity, prevLineItem } = gtmEvent
+      const removeEvent = () => setGtmEvents({ action: 'remove', event: gtmEvent })
+
+      //find line item
+      let lineItem
+      if (!lineItem && prevLineItem) lineItem = prevLineItem
+      if (!lineItem && lineId && enrichedItems) lineItem = enrichedItems.find((li) => li.id === lineId)
+      if (!lineItem && variantId && enrichedItems) lineItem = enrichedItems.find((li) => li.variant_id === variantId)
+      if (!lineItem) return
+
+      const product = lineItem.variant.product;
+      if (!product) return removeEvent()
+
+      let catCount = 1
+      const categories: Record<string, string> = {}
+      if (product.categories?.length) for (const cat of product.categories) {
+        if (catCount > 5) break
+        categories[`item_category${catCount > 1 ? catCount : ""}`] = cat.handle
+        catCount++
+      }
+
+      let eventName: Gtag.EventNames | undefined
+      const previousQuantity = lineItem.quantity
+      if (eventType === "add") {
+        eventName = "add_to_cart"
+      }
+      if (eventType === "delete") {
+        eventName = "remove_from_cart"
+      }
+      if (eventType === "update") {
+        if (quantity < 0) {
+          eventName = "remove_from_cart"
+        }
+        if (quantity > 0) {
+          eventName = "add_to_cart"
+        }
+      }
+      if (!eventName) return removeEvent()
+
+      const variant = lineItem.variant as CalculatedVariant
+      const price = Number(variant.calculated_price) / 100
+      const oldPrice = Number(variant.original_price) / 100
+      const discount = oldPrice - price
+      const eventQuantity = Math.abs(quantity)
+
+      const item= {
+        ...categories,
+        item_name: product.title,
+        item_id: lineItem.variant.sku,
+        quantity: eventQuantity,
+        item_variant: lineItem.variant.title,
+        discount: discount > 0 ? discount : undefined,
+        price,
+      }
+
+      if (discount > 0) item.discount = 3.00
+
+      sendGtmEcommerceEvent(
+        eventName,
+        {
+          currency: cart?.region?.currency_code.toUpperCase(),
+          value: price * eventQuantity,
+          items: [item],
+        }
+      )
+
+      removeEvent()
+    }
+
+    gtmEvents.forEach((gtmEvent) => {
+      sendGtmEvent(gtmEvent)
+    })
+
+  }, [gtmEvents, cart, enrichedItems])
+
+
   const addItem = ({
     variantId,
     quantity,
@@ -243,6 +344,7 @@ export const StoreProvider = ({ children }: StoreProps) => {
           setCart(cart)
           storeCart(cart.id)
           timedOpen()
+          setGtmEvents({ action: 'add', event: { id: nanoid(), eventType: "add", variantId, quantity } })
         },
         onError: (error) => {
           handleError(error)
@@ -252,12 +354,15 @@ export const StoreProvider = ({ children }: StoreProps) => {
   }
 
   const deleteItem = (lineId: string) => {
+    const prevLineItem = enrichedItems?.find((li) => li.id === lineId)
+    const quantity = prevLineItem?.quantity || 0
     removeLineItem.mutate(
       {
         lineId,
       },
       {
         onSuccess: ({ cart }) => {
+          if (prevLineItem) setGtmEvents({ action: 'add', event: { id: nanoid(), eventType: "delete", lineId, quantity, prevLineItem } })
           setCart(cart)
           storeCart(cart.id)
         },
@@ -275,6 +380,7 @@ export const StoreProvider = ({ children }: StoreProps) => {
     lineId: string
     quantity: number
   }) => {
+    const previousQuantity = cart?.items?.find((li) => li.id === lineId)?.quantity || 0
     adjustLineItem.mutate(
       {
         lineId,
@@ -282,6 +388,7 @@ export const StoreProvider = ({ children }: StoreProps) => {
       },
       {
         onSuccess: ({ cart }) => {
+          setGtmEvents({ action: 'add', event: { id: nanoid(), eventType: "update", lineId, quantity: quantity - previousQuantity } })
           setCart(cart)
           storeCart(cart.id)
         },
