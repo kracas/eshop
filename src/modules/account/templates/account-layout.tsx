c90b9bfc16e3681177a8d1aca88e89e2d5885909
kracas <karolis@lobot.co>
2023-08-01 09:07:25 +0000

    remove customer service info from account template

--- src/modules/account/templates/account-layout.tsx
+++ src/modules/account/templates/account-layout.tsx
@@ -28,20 +28,6 @@ const AccountLayout: React.FC = ({ children }) => {
           </div>
           <div className="flex-1">{children}</div>
         </div>
-        <div className="flex flex-col small:flex-row items-end justify-between small:border-t border-gray-200 px-8 py-12 gap-x-8">
-          <div>
-            <h3 className="text-xl-semi mb-4">Got questions?</h3>
-            <span className="text-small-regular">
-              You can find frequently asked questions and answers on our
-              customer service page.
-            </span>
-          </div>
-          <div>
-            <UnderlineLink href="/customer-service">
-              Customer Service
-            </UnderlineLink>
-          </div>
-        </div>
       </div>
     </div>
   )
